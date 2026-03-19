bbd73d08d7c140cc499eb0dca906a17dede01a42
azoladyantyi <aizydyantyi@gmail.com>
2026-03-19 04:38:05 +0200

    adding factorial tests and fix browser setup

--- src/test/java/ExtentReports/ExtentReportManager.java
+++ src/test/java/ExtentReports/ExtentReportManager.java
@@ -1,4 +1,31 @@
 package ExtentReports;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+
+import java.io.File;
+
 public class ExtentReportManager {
+
+    private static String reportDir=System.getProperty("user.dir") +"/target/reports/FactorialApplicationReport.html";
+
+    private static ExtentReports extentReports;
+    private static ExtentSparkReporter extentSparkReporter;
+
+    public static ExtentReports extentReports(){
+        extentReports = new ExtentReports();
+        extentSparkReporter = new ExtentSparkReporter(new File(reportDir));
+        extentReports.attachReporter(extentSparkReporter);
+
+        extentSparkReporter.config().setDocumentTitle("Extent Report Title");
+        extentSparkReporter.config().setTheme(Theme.DARK);
+        extentSparkReporter.config().setReportName("Ndosi Automation Report Name");
+
+        extentReports.setSystemInfo("OS", System.getProperty("os.name"));
+        extentReports.setSystemInfo("Execution Machine", System.getProperty("user.name"));
+
+        return extentReports;
+    }
+
 }
