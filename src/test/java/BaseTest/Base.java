fb0c7da2dba2de7e070c352dc071815f07af0e40
azoladyantyi <aizydyantyi@gmail.com>
2026-03-19 04:31:25 +0200

    added a code that will allow the browser to be display

--- src/test/java/BaseTest/Base.java
+++ src/test/java/BaseTest/Base.java
@@ -1,4 +1,35 @@
 package BaseTest;
+import Page.NegativeTestScript;
+import Page.PositiveTestScripts;
+import Utilities.BrowserFactory;
+import Utilities.Screenshots;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
 
 public class Base {
+    BrowserFactory browserFactory = new BrowserFactory();
+    public final String url = "https://qainterview.pythonanywhere.com/";
+    public final String browserChoice = "chrome";
+
+    public BaseTest.WebDriver driver;
+    public PositiveTestScripts factorialCalculatorScreen;
+    public NegativeTestScript negativeTestScript;
+    public Screenshots screenshots;
+
+
+    @BeforeClass
+    public void setUp() {
+        driver = browserFactory.startBrowser(browserChoice, url);
+        factorialCalculatorScreen  =new PositiveTestScripts(driver);
+        screenshots = new Screenshots();
+    }
+
+    @AfterClass
+    public void tearDown() {
+        browserFactory.closeBrowser();
+    }
+
 }
+
--- src/test/java/Utilities/BrowserFactory.java
+++ src/test/java/Utilities/BrowserFactory.java
@@ -1,4 +1,37 @@
 package Utilities;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+import org.testng.annotations.Test;
+
 public class BrowserFactory {
+
+    static WebDriver driver;
+
+    public static WebDriver startBrowser(String browserChoice, String url) {
+        if (browserChoice.equalsIgnoreCase("chrome")) {
+            driver = new ChromeDriver();
+        } else if (browserChoice.equalsIgnoreCase("firefox")) {
+            driver = new FirefoxDriver();
+        } else if (browserChoice.equalsIgnoreCase("edge")) {
+            EdgeOptions edgeOptions = new EdgeOptions();
+            edgeOptions.addArguments("--headless");
+            driver = new EdgeDriver(edgeOptions);
+        } else {
+            driver = new SafariDriver();
+        }
+        driver.manage().window().maximize();
+        driver.get(url);
+        return driver;
+    }
+    public void closeBrowser() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 }
